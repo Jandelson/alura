d7c913c854c4728a863709228ecfcd54f1df3ad9
Jandelson Oliveira <jandelson_olivira@yahoo.com.br>
2022-09-17 11:13:20 -0300

    TESTE

--- php/buscar/buscar-cursos.php
+++ php/buscar/buscar-cursos.php
@@ -2,6 +2,9 @@
 
 use GuzzleHttp\Client;
 
+require __DIR__ . '/vendor/autoload.php';
+
 $client = new Client();
 $resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
 $html = $resposta->getBody();
+echo $html;
